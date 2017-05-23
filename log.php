7fd7aacae83a9201cd06bd9f6a503edbf8e51e2e
Isis Naomi Ramirez <isisnaomi@gmail.com>
2017-05-23 15:42:00 -0500

    Log

---
+++ log.php
@@ -0,0 +1,83 @@
+<html>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container-fluid">
+      <div class="row">
+        <?php include 'layout/sidebar.php';?>
+        <div class="col-sm-10 main">
+          <div class="col-md-12">
+            <div class="">
+
+              <h3>Log</h3>
+
+            </div>
+          </div>
+          <div class="container">
+            <div class="row justify-content-start log--header">
+
+                <div class="col-sm-2">
+                  Action
+                </div>
+                <div class="col-sm-4">
+                  Concept
+                </div>
+                <div class="col-sm-4">
+                  User
+                </div>
+                <div class="col-sm-2">
+                  Date
+                </div>
+            </div>
+          <div class="row justify-content-start log--card">
+
+              <div class="col-sm-2">
+                <span class="log--action">Accion</span>
+              </div>
+              <div class="col-sm-4">
+                Concepto - Tema
+              </div>
+              <div class="col-sm-4">
+                Usuario
+              </div>
+              <div class="col-sm-2">
+                Fecha Hora
+              </div>
+          </div>
+
+          <div class="row justify-content-start log--card">
+
+              <div class="col-sm-2">
+                <span class="log--action">Accion</span>
+              </div>
+              <div class="col-sm-4">
+                Concepto - Tema
+              </div>
+              <div class="col-sm-4">
+                Usuario
+              </div>
+              <div class="col-sm-2">
+                Fecha Hora
+              </div>
+          </div>
+        </div>
+
+
+
+        </div>
+
+
+
+
+
+      </div>
+  </div>
+</body>
+
+</html>
